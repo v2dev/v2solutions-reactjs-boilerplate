8bf815e9555617b9eb290c068f0a1ff6c791b921
Shashikant Chauhan <shashikant.chauhan@v2solutions.com>
2023-10-26 12:38:25 +0530

    eslint bypass for config file works

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+/* eslint-env node */
+
 const path = require("path")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
