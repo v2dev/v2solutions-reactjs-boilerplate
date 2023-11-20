6d06a4b26e0eb4490a676eb19bd58398b3303cbf
Shashikant Chauhan <shashikant.chauhan@v2solutions.com>
2023-11-20 15:51:15 +0530

    reset password test case for render

--- src/__tests__/ResetPw.test.js
+++ src/__tests__/ResetPw.test.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-undef */
  
 import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store'; // You may need to install this
