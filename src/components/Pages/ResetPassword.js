8ba4c22a66d0a110b317d7135169584a99276685
Shashikant Chauhan <shashikant.chauhan@v2solutions.com>
2023-11-28 14:37:52 +0530

    Token option

--- src/components/Pages/ResetPassword.js
+++ src/components/Pages/ResetPassword.js
@@ -1,53 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { resetPasswordAction } from "../../redux/auth";
+import { useParams } from "react-router-dom";
 
 function ResetPassword() {
-    // { token }
   const dispatch = useDispatch();
+  const { token } = useParams();
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  
   const [otp, setOtp] = useState('');
-  
-  
+  const [formErrors, setFormErrors] = useState({});
+
+  useEffect(() => {
+    // If a token is present in the URL, hide the OTP input field
+    if (token) {
+      setOtp('');
+    }
+  }, [token]);
+
   const error = useSelector(state => state.auth.error);
   const message = useSelector(state => state.auth.message);
-  const [formErrors, setFormErrors] = useState({});
 
   const handleResetPassword = async (e) => {
     e.preventDefault();
-    // Dispatch an action to reset the password
-    // Pass 'password' and 'confirmPassword' to the action
     if (validateForm()) {
-      dispatch(resetPasswordAction(otp,password, confirmPassword));
+      dispatch(resetPasswordAction(otp || token, password, confirmPassword));
     }
   };
 
   const validateForm = () => {
     const errors = {};
-    
-    // Validate each field
-    
-    
-    if (!otp) {
-      errors.otp = "OTP  is required";
+
+    // Validate OTP only if it is visible
+    if (!token && !otp) {
+      errors.otp = "OTP is required";
     }
+
     const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
     if (!password) {
       errors.password = "Password is required";
     } else if (!passwordRegex.test(password)) {
       errors.password = "Password must contain at least 8 characters, including one uppercase letter, one lowercase letter, and one number";
     }
-    
+
     if (!confirmPassword) {
-      errors.confirmPassword = "Confirm Pasword is required";
+      errors.confirmPassword = "Confirm Password is required";
     }
-    if (password != confirmPassword) {
-      errors.confirmPassword = "Confirm Pasword is not match";
+    if (password !== confirmPassword) {
+      errors.confirmPassword = "Confirm Password does not match";
     }
+
     setFormErrors(errors);
-    return Object.keys(errors).length === 0; // If no errors, the form is valid
+    return Object.keys(errors).length === 0;
   };
 
   const handleInputChange = (e) => {
@@ -56,7 +60,7 @@ function ResetPassword() {
       setPassword(value);
     } else if (name === 'confirmPassword') {
       setConfirmPassword(value);
-    }else if (name === 'otp') {
+    } else if (name === 'otp') {
       setOtp(value);
     }
   };
@@ -72,27 +76,52 @@ function ResetPassword() {
               <form onSubmit={handleResetPassword}>
                 {error && <p className="text-danger text-center">{error}</p>}
                 {message && <p className="text-success text-center">{message}</p>}
-                <div className="mb-3">
-                  <label htmlFor="password" className="form-label">OTP</label>
-                  <input type="number"  className={`form-control ${formErrors.otp && "is-invalid"}`} id="otp" name="otp" value={otp} onChange={handleInputChange} />
-                  <div className={`invalid-feedback ${formErrors.otp ? "d-block" : ""}`}>
+
+                {!token && (
+                  <div className="mb-3">
+                    <label htmlFor="otp" className="form-label">OTP</label>
+                    <input
+                      type="number"
+                      className={`form-control ${formErrors.otp && "is-invalid"}`}
+                      id="otp"
+                      name="otp"
+                      value={otp}
+                      onChange={handleInputChange}
+                    />
+                    <div className={`invalid-feedback ${formErrors.otp ? "d-block" : ""}`}>
                       {formErrors.otp}
                     </div>
-                </div>
+                  </div>
+                )}
+
                 <div className="mb-3">
                   <label htmlFor="password" className="form-label">New Password</label>
-                  <input type="password"  className={`form-control ${formErrors.password && "is-invalid"}`} id="password" name="password" value={password} onChange={handleInputChange} />
+                  <input
+                    type="password"
+                    className={`form-control ${formErrors.password && "is-invalid"}`}
+                    id="password"
+                    name="password"
+                    value={password}
+                    onChange={handleInputChange}
+                  />
                   <div className={`invalid-feedback ${formErrors.password ? "d-block" : ""}`}>
-                      {formErrors.password}
-                    </div>
+                    {formErrors.password}
+                  </div>
                 </div>
 
                 <div className="mb-3">
                   <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
-                  <input type="password"  className={`form-control ${formErrors.confirmPassword && "is-invalid"}`} id="confirmPassword" name="confirmPassword" value={confirmPassword} onChange={handleInputChange} />
+                  <input
+                    type="password"
+                    className={`form-control ${formErrors.confirmPassword && "is-invalid"}`}
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    value={confirmPassword}
+                    onChange={handleInputChange}
+                  />
                   <div className={`invalid-feedback ${formErrors.confirmPassword ? "d-block" : ""}`}>
-                      {formErrors.confirmPassword}
-                    </div>
+                    {formErrors.confirmPassword}
+                  </div>
                 </div>
 
                 <button type="submit" className="btn btn-primary">Submit</button>
