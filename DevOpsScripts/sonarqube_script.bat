a60788d2d1107da28028904b56bea888ed55f0db
sagarthoratv2 <sagar.thorat@v2solutions.com>
2024-01-19 15:45:54 +0530

    Adding Sonarcube configuration

--- DevOpsScripts/sonarqube_script.bat
+++ DevOpsScripts/sonarqube_script.bat
@@ -1,5 +1,5 @@
 @echo off
 set SCANNER_HOME=%1
-set %PROJECT_KEY%=%2
+set PROJECT_KEY=%2
 %SCANNER_HOME%\bin\sonar-scanner -Dsonar.login=%SONARQUBE_CREDENTIALS% -Dsonar.projectKey=%PROJECT_KEY%
 %SCANNER_HOME%\bin\sonar-scanner.bat -D"sonar.projectKey=Reactjs" -D"sonar.sources=." -D"sonar.host.url=${SONARQUBE_SERVER}" -D"sonar.login=sqp_7cc24242be902c251b7796c4512b1620da638125"
