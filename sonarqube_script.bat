1dd533c0b06985a9493908fdb2d8a484c890aa22
sagarthoratv2 <sagar.thorat@v2solutions.com>
2024-01-22 11:42:26 +0530

    Cleanup

--- sonarqube_script.bat
+++ sonarqube_script.bat
@@ -2,5 +2,4 @@
 set SCANNER_HOME=%1
 set PROJECT_KEY=%2
 %SCANNER_HOME%\bin\sonar-scanner -Dsonar.login=%SONARQUBE_CREDENTIALS% -Dsonar.projectKey=%PROJECT_KEY%
-@REM -Dsonar.sources=%CD%
 %SCANNER_HOME%\bin\sonar-scanner.bat -D"sonar.projectKey=Reactjs" -D"sonar.sources=." -D"sonar.host.url=${SONARQUBE_SERVER}" -D"sonar.token=sqp_7cc24242be902c251b7796c4512b1620da638125"
